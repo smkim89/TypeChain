1f26a2a6fe1a2dc5f8e13ec67b197ce5301989e4
smkim89 <kiak357@gmail.com>
2018-07-30 23:53:46 +0900

    블록 추가 및 블록 연결.

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,63 @@
+import * as CryptoJS from "crypto-js";
+
 interface Human {
     name : string;
     age : number;
     gender : string;
 }
 
+class People {
+    public name : string;
+    public age : number;
+    public gender : string;
+
+    constructor(name : string, age : number, gender:string){
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
+}
+
+class Block {
+    public index: number;
+    public hash: string;
+    public previousHash: string;
+    public data: string;
+    public timestamp: number;
+
+    static calculateBlockHash = (
+        index: number,
+        previousHash: string,
+        timestamp: number,
+        data: string
+      ): string =>
+        CryptoJS.SHA256(index + previousHash + timestamp + data).toString();
+
+    constructor(
+      index: number,
+      hash: string,
+      previousHash: string,
+      data: string,
+      timestamp: number
+    ) {
+      this.index = index;
+      this.hash = hash;
+      this.previousHash = previousHash;
+      this.data = data;
+      this.timestamp = timestamp;
+    }
+  }
+
+
 const person = {
     name : "eunmi",
     age : 29,
     gender : "F" 
 }
-const sayHiObject = (person : Human): string => {
+
+//Class로 선언도 가능하고 Interface로 선언도 가능하다.
+//클래스로 선언한경우 컴파일 후 클래스 내용이 js에 남는다.
+const sayHiObject = (person : People): string => {
     return `Hello ${person.name} , age ${person.age}, gender ${person.gender}!`;
 }
 
@@ -21,5 +69,41 @@ const sayHi = (name:string, age:number, gender:string): void => {
 
 sayHi('sungmin', 30, 'M');
 console.log(sayHiObject(person));
+
+
+const genesisBlock: Block = new Block(0, "2020202020202", "", "Hello", 123456);
+let blockchain: Block[] = [genesisBlock];;
+console.log(blockchain);
+
+const getBlockChain = () : Block[] => blockchain;
+
+const getLastBlock = () : Block => blockchain[blockchain.length -1];
+
+const getTimeStamp = () : number => Math.round(new Date().getTime() / 1000);
+
+const createNewBlock = (data: string): Block => {
+    const previosBlock: Block = getLastBlock();
+    const newIndex: number = previosBlock.index + 1;
+    const newTimestamp: number = getTimeStamp();
+    const newHash: string = 
+        Block.calculateBlockHash(
+        newIndex,
+        previosBlock.hash,
+        newTimestamp,
+        data
+        );
+    const newBlock: Block = 
+        new Block(
+        newIndex,
+        newHash,
+        previosBlock.hash,
+        data,
+        newTimestamp
+        );
+    return newBlock;
+  };
+
+  console.log(createNewBlock("hello"));
+  console.log(createNewBlock("bye bye"));
 
 export {};
